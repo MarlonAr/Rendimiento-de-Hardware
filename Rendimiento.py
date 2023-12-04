3626a4832f14c3ff34885b1417d76d5967bb61eb
MarlonUCE <106186299+MarlonUCE@users.noreply.github.com>
2023-12-03 23:41:19 -0500

    Rendimiento.py

--- Rendimiento.py
+++ Rendimiento.py
@@ -1,18 +1,18 @@
 import psutil
 
-def get_cpu_usage():
+def obtener_rendimiento_cpu():
     # Obtiene el uso del CPU en porcentaje
-    return psutil.cpu_percent()
+    return psutil.cpu_percent(interval=1)
 
-def get_memory_usage():
+def obtener_rendimiento_memoria():
     # Obtiene el uso de la memoria en porcentaje
     return psutil.virtual_memory().percent
 
-def get_network_usage():
-    # Obtiene el uso de la red en porcentaje
+def obtener_rendimiento_red():
+     # Obtiene el uso de la red en porcentaje
     return psutil.net_io_counters().bytes_recv / 1024**3
 
-def get_cpu_temperature():
+def obtener_temperatura_cpu():
     # Obtiene la temperatura del CPU en grados Celsius
     try:
         temps = psutil.sensors_temperatures()
@@ -27,14 +27,11 @@ def get_cpu_temperature():
 
 if __name__ == "__main__":
     # Muestra el rendimiento del CPU
-    print("Uso del CPU:", get_cpu_usage())
-
+    print("Rendimiento del CPU (%):", obtener_rendimiento_cpu())
     # Muestra el rendimiento de la memoria
-    print("Uso de la memoria:", get_memory_usage())
-
+    print("Rendimiento de la Memoria (%):", obtener_rendimiento_memoria())
     # Muestra el rendimiento de la red
-    print("Uso de la red:", get_network_usage())
-
+    print("Rendimiento de la Red (%):", obtener_rendimiento_red())
     # Muestra la temperatura del CPU
-    print("Temperatura del CPU:", get_cpu_temperature())
+    print("Temperatura del CPU:", obtener_temperatura_cpu())
 
